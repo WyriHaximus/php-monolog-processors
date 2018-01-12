325524204b30656296c9c53a5be05b9c4b8f66d9
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-01-12 08:07:02 +0100

    test exception trace

--- tests/TraceProcessorTest.php
+++ tests/TraceProcessorTest.php
@@ -3,20 +3,39 @@
 namespace WyriHaximus\Tests\Monolog\Processors;
 
 use PHPUnit\Framework\TestCase;
-use WyriHaximus\Monolog\Processors\RuntimeProcessor;
 use WyriHaximus\Monolog\Processors\TraceProcessor;
 
 final class TraceProcessorTest extends TestCase
 {
-    public function testKeyValue()
+    public function testNonExceptionTrace()
     {
         $processor = new TraceProcessor();
         $line = __LINE__ + 1;
         $record = $processor([]);
 
+        self::assertTrue(isset($record['extra']['trace']));
         self::assertTrue(isset($record['extra']['trace'][0]));
         self::assertSame(__FILE__, $record['extra']['trace'][0]['file']);
         self::assertSame($line, $record['extra']['trace'][0]['line']);
+        self::assertThat(count($record['extra']['trace']), self::greaterThan(1));
+    }
+
+    public function testExceptionTrace()
+    {
+        $processor = new TraceProcessor();
+        $exception = new \Exception('fail!');
+        $record = $processor([
+            'context' => [
+                'exception' => $exception,
+            ],
+        ]);
+
+        self::assertTrue(isset($record['extra']['trace']));
+        self::assertTrue(isset($record['extra']['trace'][0]));
+        self::assertTrue(!isset($record['extra']['trace'][0]['file']));
+        self::assertTrue(!isset($record['extra']['trace'][0]['line']));
+        self::assertSame(__FUNCTION__, $record['extra']['trace'][0]['function']);
+        self::assertSame(__CLASS__, $record['extra']['trace'][0]['class']);
         self::assertThat(count($record['extra']['trace']), self::greaterThan(1));
     }
 }
